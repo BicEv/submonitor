35ce1de6742de1165ab9c7d278d8f66223f0e957
BicEv <bickbovev@gmail.com>
2026-03-25 13:26:31 +0300

    Basic project structure

---
+++ src/main/java/ru/bicev/submonitor/SubmonitorApplication.java
@@ -0,0 +1,13 @@
+package ru.bicev.submonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bicev/submonitor/SubmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bicev.submonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
