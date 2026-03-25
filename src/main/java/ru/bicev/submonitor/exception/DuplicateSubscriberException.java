9ab4216a2ca0f01ddcda3d048e6c8efdd19cc7ce
BicEv <bickbovev@gmail.com>
2026-03-25 18:50:54 +0300

    DuplicateSubscriberException and NotFoundException

---
+++ src/main/java/ru/bicev/submonitor/exception/DuplicateSubscriberException.java
@@ -0,0 +1,9 @@
+package ru.bicev.submonitor.exception;
+
+public class DuplicateSubscriberException extends RuntimeException {
+
+    public DuplicateSubscriberException(String message) {
+        super(message);
+    }
+
+}
