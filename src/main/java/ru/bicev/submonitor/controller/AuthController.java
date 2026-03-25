888ce8d22ec4e4f267b4b1e08de3d9cd2dbc9552
BicEv <bickbovev@gmail.com>
2026-03-25 20:21:54 +0300

    AuthController

---
+++ src/main/java/ru/bicev/submonitor/controller/AuthController.java
@@ -0,0 +1,38 @@
+package ru.bicev.submonitor.controller;
+
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ru.bicev.submonitor.dto.auth.JwtResponse;
+import ru.bicev.submonitor.dto.auth.LoginRequest;
+import ru.bicev.submonitor.dto.auth.SingupRequest;
+import ru.bicev.submonitor.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<JwtResponse> register(@Valid @RequestBody SingupRequest request) {
+        var created = authService.registerSubscriber(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        var response = authService.login(loginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+}
