cac037ff3ccd6d55f4216dd31d5f46a3d416cc3a
BicEv <bickbovev@gmail.com>
2026-03-26 17:24:55 +0300

    Added neccessary methods in SubscriptionRepository

--- src/main/java/ru/bicev/submonitor/repository/SubscriptionRepository.java
+++ src/main/java/ru/bicev/submonitor/repository/SubscriptionRepository.java
@@ -1,6 +1,7 @@
 package ru.bicev.submonitor.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +9,8 @@ import ru.bicev.submonitor.entity.Subscription;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
-    List<Subscription> findAllBySubscriberId(Long subscriberId);
+    List<Subscription> findAllBySubscriberIdAndIsDeletedFalse(Long subscriberId);
+
+    Optional<Subscription> findByIdAndSubscriberId(Long id, Long subscriberId);
 
 }
