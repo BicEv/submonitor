25924bc1b07921e4f935b8d6fa96c3b05ad791df
BicEv <bickbovev@gmail.com>
2026-03-26 19:24:03 +0300

    Changed method to support Pageable and added entityGraph to avoid N+1 when loading connected entities

--- src/main/java/ru/bicev/submonitor/repository/PaymentLogRepository.java
+++ src/main/java/ru/bicev/submonitor/repository/PaymentLogRepository.java
@@ -1,13 +1,20 @@
 package ru.bicev.submonitor.repository;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ru.bicev.submonitor.entity.PaymentLog;
 
 public interface PaymentLogRepository extends JpaRepository<PaymentLog, Long> {
 
-    List<PaymentLog> findAllBySubscriberId(Long subscriberId);
+    @EntityGraph(attributePaths = { "subscription", "subscription.service" })
+    Page<PaymentLog> findAllBySubscriberId(Long subscriberId, Pageable pageable);
+
+    @EntityGraph(attributePaths = { "subscription", "subscription.service" })
+    Optional<PaymentLog> findByIdAndSubscriberID(Long id, Long subscriberId);
 
 }
