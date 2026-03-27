a6661ce81243e173bf2b0c542ffd3536a9221fe5
BicEv <bickbovev@gmail.com>
2026-03-27 14:01:01 +0300

    Added method to retrieve all active subscriptions which are expired

--- src/main/java/ru/bicev/submonitor/repository/SubscriptionRepository.java
+++ src/main/java/ru/bicev/submonitor/repository/SubscriptionRepository.java
@@ -1,8 +1,10 @@
 package ru.bicev.submonitor.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ru.bicev.submonitor.entity.Subscription;
@@ -12,5 +14,8 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
     List<Subscription> findAllBySubscriberIdAndIsDeletedFalse(Long subscriberId);
 
     Optional<Subscription> findByIdAndSubscriberId(Long id, Long subscriberId);
+
+    @EntityGraph(attributePaths = "subscriber")
+    List<Subscription> findAllByIsActiveTrueAndIsDeletedFalseAndNextPaymentDateLessThanEqual(LocalDate date);
 
 }
