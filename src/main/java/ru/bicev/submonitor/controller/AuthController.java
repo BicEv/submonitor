4f9e4e118598e06fcfd2cc44fa2e9c3d587675fb
BicEv <bickbovev@gmail.com>
2026-03-30 19:01:03 +0300

    Added javadoc

--- src/main/java/ru/bicev/submonitor/controller/AuthController.java
+++ src/main/java/ru/bicev/submonitor/controller/AuthController.java
@@ -1,6 +1,5 @@
 package ru.bicev.submonitor.controller;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +14,9 @@ import ru.bicev.submonitor.dto.auth.LoginRequest;
 import ru.bicev.submonitor.dto.auth.SingupRequest;
 import ru.bicev.submonitor.service.AuthService;
 
+/**
+ * Контроллер для регистрации и логина пользователя
+ */
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -22,12 +24,28 @@ public class AuthController {
 
     private final AuthService authService;
 
+    /**
+     * Метод регистрирующий нового пользователя
+     * 
+     * @param request запрос на создание нового пользователя, содержащий юзернейм,
+     *                email и пароль
+     * @return ответ с jwt-токеном, идентификатором созданного пользователя и его
+     *         юзернеймом
+     */
     @PostMapping("/register")
     public ResponseEntity<JwtResponse> register(@Valid @RequestBody SingupRequest request) {
         var created = authService.registerSubscriber(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
+    /**
+     * Метод для авторизации пользователя и получения jwt-токена
+     * 
+     * @param loginRequest запрос на авторизацию пользователя, содержащий юзернейм и
+     *                     пароль
+     * @return ответ с jwt-токеном, идентификатором созданного пользователя и его
+     *         юзернеймом
+     */
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         var response = authService.login(loginRequest);
