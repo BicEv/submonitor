cb0a9dd4172d9984b9d6e6bda7a6a7d5831c6b29
BicEv <bickbovev@gmail.com>
2026-03-31 17:20:48 +0300

    Removed PaymentLogId from request

--- src/main/java/ru/bicev/submonitor/dto/payment/PaymentUpdateLogRequest.java
+++ src/main/java/ru/bicev/submonitor/dto/payment/PaymentUpdateLogRequest.java
@@ -3,14 +3,12 @@ package ru.bicev.submonitor.dto.payment;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.NotNull;
 import ru.bicev.submonitor.entity.enums.PaymentStatus;
 
 public record PaymentUpdateLogRequest(
-        @NotNull(message = "Payment id cannot be null") Long id,
-        Long subscriptionId,
-        BigDecimal amount,
-        LocalDate paymentDate,
-        PaymentStatus status) {
+                Long subscriptionId,
+                BigDecimal amount,
+                LocalDate paymentDate,
+                PaymentStatus status) {
 
 }
