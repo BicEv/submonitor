ebee03a8d8af12dd56246f14c9b92cfdfc718fff
BicEv <bickbovev@gmail.com>
2026-04-03 18:22:55 +0300

    Removed spaces between : and QueryParam

--- src/main/java/ru/bicev/submonitor/repository/ServiceRepository.java
+++ src/main/java/ru/bicev/submonitor/repository/ServiceRepository.java
@@ -14,7 +14,7 @@ public interface ServiceRepository extends JpaRepository<Service, Long> {
     @Query("SELECT s FROM Service s WHERE s.owner.id IS NULL OR s.owner.id =: ownerId")
     List<Service> findAllAvailable(@Param("ownerId") Long ownerId);
 
-    @Query("SELECT s FROM Service s WHERE s.id =: id AND (s.owner.id IS NULL OR s.owner.id =: ownerId)")
+    @Query("SELECT s FROM Service s WHERE s.id = :id AND (s.owner.id IS NULL OR s.owner.id = :ownerId)")
     Optional<Service> findByIdAndOwnerIdEqualsOrNull(@Param("id") Long id, @Param("ownerId") Long ownerId);
 
     Optional<Service> findByIdAndOwnerId(Long id, Long ownerId);
